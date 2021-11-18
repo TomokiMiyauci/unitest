c13ea1bf4469e1936bbbd78f14077bf96f154976
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-18 19:29:13 +0900

    chore: add copyright writer to husky hooks

--- deps.ts
+++ deps.ts
@@ -1,2 +1,5 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
-export { AssertionError } from "https://deno.land/std@0.115.1/testing/asserts.ts";
+export {
+  AssertionError,
+  equal,
+} from "https://deno.land/std@0.115.1/testing/asserts.ts";
