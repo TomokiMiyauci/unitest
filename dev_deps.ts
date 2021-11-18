e3e48fc8960ac05556ac2756528ab0bd1ede4335
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-18 19:55:05 +0900

    test: add custom assertion

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,5 +1,15 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
-export {
-  assert,
-  assertEquals,
-} from "https://deno.land/std@0.115.1/testing/asserts.ts";
+export { assert } from "https://deno.land/std@0.115.1/testing/asserts.ts";
+
+import { assertEquals } from "https://deno.land/std@0.115.1/testing/asserts.ts";
+import type { MatchResult } from "@matcher/types.ts";
+
+function assertSuccess({ pass }: MatchResult): void {
+  assertEquals(pass, true);
+}
+
+function assertFail({ pass }: MatchResult): void {
+  assertEquals(pass, false);
+}
+
+export { assertEquals, assertFail, assertSuccess };
--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
 import { defineExpect } from "@/expect.ts";
-import { fail, jestMatcher, success, toBe } from "@matcher/mod.ts";
+export * from "@matcher/mod.ts";
+import { jestMatcher } from "@matcher/mod.ts";
 export type { Expected, MatcherMap } from "@/expect.ts";
-export type { Matcher, MatchResult } from "@/matcher/mod.ts";
 
 const expect = defineExpect(jestMatcher);
 
-export { defineExpect, expect, fail, jestMatcher, success, toBe };
+export { defineExpect, expect };
