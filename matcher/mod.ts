c946e40d5d3185dc81a1f0429a9fc63b936a4d59
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-18 19:54:11 +0900

    feat(matcher): add `toBeFalsy` matcher

--- matcher/mod.ts
+++ matcher/mod.ts
@@ -1,12 +1,14 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
 import { toBe } from "@matcher/to_be.ts";
 import { toEqual } from "@matcher/to_equal.ts";
+import { toBeFalsy } from "@matcher/to_be_falsy.ts";
 export * from "@matcher/types.ts";
 export * from "@matcher/utils.ts";
 
 const jestMatcher = {
   toBe,
   toEqual,
+  toBeFalsy,
 };
 
-export { jestMatcher, toBe, toEqual };
+export { jestMatcher, toBe, toBeFalsy, toEqual };
