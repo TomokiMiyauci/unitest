2f542b4be5f972cdd584f34ad6e9c43fcc7edcd0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-19 10:55:30 +0900

    refactor(expect): move expect to under expect dir

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
-import { defineExpect } from "@/expect.ts";
+import { defineExpect } from "@expect/mod.ts";
 export * from "@matcher/mod.ts";
 import { jestMatcher } from "@matcher/mod.ts";
-export type { Expected, MatcherMap } from "@/expect.ts";
+export type { Expected, MatcherMap } from "@expect/mod.ts";
 
 const expect = defineExpect(jestMatcher);
 
