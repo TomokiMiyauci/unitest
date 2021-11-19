fef3c7e3d83f294f35bd1a39ba906fbd9c1fff9c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-20 08:55:41 +0900

    refactor(matcher): change matcher export point

--- matcher/mod.ts
+++ matcher/mod.ts
@@ -2,3 +2,35 @@
 export * from "@matcher/preset.ts";
 export * from "@matcher/types.ts";
 export * from "@matcher/utils.ts";
+
+export { toBe } from "@matcher/to_be.ts";
+export { toEqual } from "@matcher/to_equal.ts";
+export { toBeFalsy } from "@matcher/to_be_falsy.ts";
+export { toBeTruthy } from "@matcher/to_be_truthy.ts";
+export { toBeUndefined } from "@matcher/to_be_undefined.ts";
+export { toBeDefined } from "@matcher/to_be_defined.ts";
+export { toBeNull } from "@matcher/to_be_null.ts";
+export { toBeAnything } from "@matcher/to_be_anything.ts";
+export { toBeNaN } from "@matcher/to_be_nan.ts";
+export { toBeInstanceOf } from "@matcher/to_be_instance_of.ts";
+export { toBeGreaterThan } from "@matcher/to_be_greater_than.ts";
+export { toBeGreaterThanOrEqual } from "@matcher/to_be_greater_than_or_equal.ts";
+export { toBeLessThan } from "@matcher/to_be_less_than.ts";
+export { toBeLessThanOrEqual } from "@matcher/to_be_less_than_or_equal.ts";
+export { toBeCloseTo } from "@matcher/to_be_close_to.ts";
+export { toMatch } from "@matcher/to_match.ts";
+export { toContain } from "@matcher/to_contain.ts";
+export { toThrow } from "@matcher/to_throw.ts";
+export { toHaveBeenCalled } from "@matcher/to_have_been_called.ts";
+export { toHaveBeenCalledTimes } from "@matcher/to_have_been_called_times.ts";
+export { toHaveBeenCalledWith } from "@matcher/to_have_been_called_with.ts";
+export { toHaveBeenLastCalledWith } from "@matcher/to_have_been_last_called_with.ts";
+export { toHaveBeenNthCalledWith } from "@matcher/to_have_been_nth_called_with.ts";
+export { toHaveLength } from "@matcher/to_have_length.ts";
+export { toHaveReturned } from "@matcher/to_have_returned.ts";
+export { toHaveReturnedTimes } from "@matcher/to_have_returned_times.ts";
+export { toHaveReturnedWith } from "@matcher/to_have_returned_with.ts";
+export { toHaveLastReturnedWith } from "@matcher/to_have_last_returned_with.ts";
+export { toHaveNthReturnedWith } from "@matcher/to_have_nth_returned_with.ts";
+
+export { toBeNil } from "@matcher/to_be_nil.ts";
