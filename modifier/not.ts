dfd141ae26bbf1bb56546809adc75ddee63f6c63
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-23 23:30:07 +0900

    test(modifier): add `not` test case

--- modifier/not.ts
+++ modifier/not.ts
@@ -1,16 +1,25 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
-import type { PostModifier } from "./types.ts";
+import type {
+  PostModifier,
+  PostModifierContext,
+  PostModifierResult,
+} from "./types.ts";
+import { stringify } from "../helper/format.ts";
+
+function predict(
+  { pass, expectedValue }: PostModifierContext,
+): PostModifierResult {
+  const expected = pass ? `[not] ${stringify(expectedValue)}` : undefined;
+
+  return {
+    pass: !pass,
+    expected,
+  };
+}
 
 const not: PostModifier = {
   type: "post",
-  fn: ({ pass, expectedValue }) => {
-    const expected = pass ? `not ${expectedValue}` : expectedValue;
-
-    return {
-      pass: !pass,
-      expected,
-    };
-  },
+  fn: predict,
 };
 
-export { not };
+export { not, predict };
