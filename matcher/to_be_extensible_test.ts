fd1b8928ac7e1ced7a7b7b07b0edbf87a193278b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-24 12:19:45 +0900

    chore(matcher): fix typo

--- matcher/to_be_extensible_test.ts
+++ matcher/to_be_extensible_test.ts
@@ -1,5 +1,4 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
-// This module is browser compatible.
 
 import { assertExpected, assertFail, assertSuccess } from "../dev_deps.ts";
 import { toBeExtensible } from "./to_be_extensible.ts";
