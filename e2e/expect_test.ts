8c52b33ee04e5c7a499be944f9728ca77272b40a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-25 10:13:04 +0900

    test(e2e): add `toHaveProperty` test case

---
+++ e2e/expect_test.ts
@@ -0,0 +1,13 @@
+// Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
+import { defineExpect, it, toHaveProperty } from "../mod.ts";
+
+const expect = defineExpect({
+  matcherMap: {
+    toHaveProperty,
+  },
+  modifierMap: {},
+});
+
+it("should not occur error", () => {
+  expect({ a: "" }).toHaveProperty("a");
+});
