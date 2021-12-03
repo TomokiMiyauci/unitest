9da9af60875f2eac1166ba5043274dece0707da1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-12-04 01:09:58 +0900

    refactor: move fake object test

--- _e2e/expect_test.ts
+++ _e2e/expect_test.ts
@@ -1,18 +1,5 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
-import {
-  any,
-  anyNumber,
-  anyString,
-  anything,
-  defineExpect,
-  expect,
-  objectContaining,
-  stringContaining,
-  stringMatching,
-  test,
-  toStartWith,
-  toThrow,
-} from "../mod.ts";
+import { defineExpect, expect, test, toStartWith, toThrow } from "../mod.ts";
 
 test({
   name: "should not occur error",
@@ -33,46 +20,4 @@ test({
 
     expect("abcde").toStartWith("abc");
   },
-});
-
-test("anything", () => {
-  expect({}).toEqual(anything());
-});
-
-test("objectContaining", () => {
-  expect({ a: 1, b: "any" }).toEqual(
-    objectContaining({ a: any(Number), b: any(String) }),
-  );
-});
-
-test("should be match pattern", () => {
-  expect("hello! This is a good day.").toEqual(stringMatching(/good/));
-});
-
-test("should contain pattern", () => {
-  expect("hello! This is a good day.").toEqual(stringContaining("good"));
-});
-
-test("should any string", () => {
-  expect("hello! This is a good day.").toEqual(anyString());
-});
-
-test("should any number", () => {
-  expect(1).toEqual(anyNumber());
-});
-
-test("should any number", () => {
-  expect({ a: 1 }).toEqual({ a: anyNumber() });
-});
-
-test("should any object", () => {
-  expect({}).toEqual(any(Object));
-});
-
-test("should {} object", () => {
-  expect({ a: {} }).toEqual({ a: {} });
-});
-
-test("should { a: {} } object", () => {
-  expect({ a: {} }).toEqual({ a: any(Object) });
 });
