0f38afcb74052be1bb589d0bf59319d0a7efc1f5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-12-11 10:30:12 +0900

    fix(matcher): improve assertion message for `toHaveReturned`
    close #46

--- matcher/to_have_returned.ts
+++ matcher/to_have_returned.ts
@@ -1,11 +1,13 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
 // This module is browser compatible.
+
+import { isTypeReturn, pickValue } from "../mock/utils.ts";
 import type { MatchResult } from "./types.ts";
 import type { MockObject, MockResult } from "../mock/mock.ts";
 
 /** predict for `toHaveReturned` */
 function predict(mockResults: readonly MockResult[]): boolean {
-  return mockResults.some(({ type }) => type === "return");
+  return mockResults.some(isTypeReturn);
 }
 
 /** Use `.toHaveReturned` that the mock object successfully returned at least one
@@ -22,10 +24,12 @@ function predict(mockResults: readonly MockResult[]): boolean {
  * ```
  */
 function toHaveReturned({ mock }: MockObject): MatchResult {
-  // TODO:(miyauci) improve assertion message
   return {
+    actual: mock.results.filter(isTypeReturn).map(pickValue),
+    actualHint: "Actual returned:",
     pass: predict(mock.results),
-    expected: "0 < mock calls",
+    expectedHint: "Expected returned:",
+    expected: "0 < Actual.length",
   };
 }
 
