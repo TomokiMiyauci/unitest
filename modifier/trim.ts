510d5ca467cfaa4eeee10b574e4cf554517172cc
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-12-20 23:05:26 +0900

    docs(modifier): add comment for module compat

--- modifier/trim.ts
+++ modifier/trim.ts
@@ -1,4 +1,6 @@
 // Copyright 2021-Present the Unitest authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
 import type { PreModifier } from "./types.ts";
 
 /** Use `.trim` to removes the leading and trailing white space and line terminator
